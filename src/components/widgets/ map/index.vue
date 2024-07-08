53f939df7d91227a32b683d3432b2f897718e9e7
oLNidfwworld <kuvetov@bk.ru>
2024-07-08 11:34:12 +0300

    feat: clck out

--- src/components/widgets/ map/index.vue
+++ src/components/widgets/ map/index.vue
@@ -3,7 +3,7 @@ import type { LocationElement } from '@/helpers/types/enitities/locationElement'
 import { YandexMap, YandexMapClusterer, YandexMapControl, YandexMapControls, YandexMapDefaultFeaturesLayer, YandexMapDefaultSchemeLayer, YandexMapGeolocationControl, YandexMapMarker, YandexMapZoomControl } from 'vue-yandex-maps';
 import type { LngLat } from '@yandex/ymaps3-types';
 import { ref } from 'vue';
-
+import { vOnClickOutside } from '@vueuse/components'
 
 interface IProps {
     elements: LocationElement[] | undefined
@@ -29,13 +29,12 @@ const openMarker = ref<null | string>(null);
         <YandexMapDefaultFeaturesLayer />
         <YandexMapClusterer v-if="elements && elements?.length > 0" zoom-on-cluster-click>
             <YandexMapMarker v-for="(element) in elements" :key="element.id" :settings="{
-                coordinates: (element.properties.mapPlacemark as LngLat), 
-                onClick : ( ) => { openMarker = element.id }
+                coordinates: (element.properties.mapPlacemark as LngLat),  
             }">
             <div class="ymap-custom-marker" >
             
-                <img src="https://romb-art.ru/upload/img/pin.svg"/>
-                <div @click.stop="openMarker = null" v-if="openMarker === element.id"
+                <img @click="openMarker = element.id" src="https://romb-art.ru/upload/img/pin.svg"/>
+                <div v-on-click-outside="() => { openMarker = null }" @click.stop="openMarker = null" v-if="openMarker === element.id"
                     class="ymap-popup" 
                 >
                     <span class="ymap-popup__title">{{  element.name }}</span><br/><br/>
